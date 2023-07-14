f77711e0abd6d2f9816231776211cace053671e1
Santiago Mesa Villa <santito79@hotmail.com>
2023-07-14 10:21:13 +0200

    Add more data to notification in SubscriptionController broadcast (backend) and service worker (frontend). Update README

--- backend/controllers/SubscriptionController.ts
+++ backend/controllers/SubscriptionController.ts
@@ -7,6 +7,7 @@ export const post = async (req: Request, res: Response, next: NextFunction): Pro
         const subscription = req.body;
         const newSubscription = await subscriptionRepository.create(subscription);
         // Send 201 - resource created
+        console.log('User suscription created...');
         res.status(201).json(newSubscription);
     } catch (err) {
         console.error(err);
@@ -18,13 +19,16 @@ export const remove = async (req: Request, res: Response, next: NextFunction): P
     try {
         const endpoint: string = req.query.endpoint?.toString();
         if (!endpoint) {
+            console.log('Endpoint not found');
             res.sendStatus(400);
             return;
         };
         const successful = await subscriptionRepository.deleteByEndpoint(endpoint);
         if (successful) {
+            console.log('Endpoint deleted');
             res.sendStatus(200);
         } else {
+            console.log('Something has gone wrong with the deletion');
             res.sendStatus(500);
         };
     } catch (err) {
@@ -35,7 +39,29 @@ export const remove = async (req: Request, res: Response, next: NextFunction): P
 
 export const broadcast = async (_req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        const notification = { title: 'Hey, I am a push notification!'};
+        const notification = {
+            title: 'Web push notification sample',
+            body: 'Hi there 😎, I am an experimental web push notification, full of a lot of nonsensical text just for you to check how long a notification can be in order to be able to get our users read what we need them to read without missing anything, so I have to keep writing until I get a big amount of characters, yuhu!',
+            icon: process.env.WPN_SAMPLE_IMAGE_1,
+            image: process.env.WPN_SAMPLE_IMAGE_1,
+            badge: process.env.WPN_SAMPLE_IMAGE_1,
+            dir: 'auto',
+            timestamp: new Date().getTime(),
+            actions: [
+                {action: 'open', title: 'Open notification', icon: process.env.WPN_SAMPLE_IMAGE_2},
+                {action: 'close', title: 'Close notification', icon: process.env.WPN_SAMPLE_IMAGE_2},
+                {action: 'gotoweb', title: 'Go to web', icon: process.env.WPN_SAMPLE_IMAGE_2}
+            ],
+            data: {
+                name: 'I could be anything',
+                anything: 35
+            },
+            tag: 'Test push',
+            requireInteraction: true,
+            renotify: true,
+            vibrate: [300, 100, 300],
+            silent: false
+        };
         const subscriptions = await subscriptionRepository.getAll();
         const notifications: Promise<SendResult>[] = [];
         subscriptions.forEach((subscription) => {
@@ -44,6 +70,7 @@ export const broadcast = async (_req: Request, res: Response, next: NextFunction
             );
         });
         await Promise.all(notifications);
+        console.log('Push notification sent from the backend.');
         res.sendStatus(200);
     } catch (err) {
         console.error(err);
--- frontend/worker.js
+++ frontend/worker.js
@@ -2,6 +2,17 @@ self.addEventListener('push', (ev) => {
     const data = ev.data.json();
     self.registration.showNotification(data.title, {
         body: data.body,
-        icon: data.icon
+        icon: data.icon,
+        image: data.image,
+        badge: data.badge,
+        dir: data.dir,
+        timestamp: data.timestamp,
+        actions: data.actions,
+        data: data.data,
+        tag: data.tag,
+        requireInteraction: data.requireInteraction,
+        renotify: data.renotify,
+        vibrate: data.vibrate,
+        silent: data.silent
     });
 });
